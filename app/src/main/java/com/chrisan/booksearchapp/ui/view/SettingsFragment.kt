309a34febd1c3e8de7cb989614c6a56e14e463bc
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-06 16:23:56 +0900

    [fix] : viewbinding 을 잘못 설정해서 bottomnavigationview 오작동

--- app/src/main/java/com/chrisan/booksearchapp/ui/view/SettingsFragment.kt
+++ app/src/main/java/com/chrisan/booksearchapp/ui/view/SettingsFragment.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.chrisan.booksearchapp.databinding.FragmentFavoriteBinding
+import com.chrisan.booksearchapp.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
-    private var _binding: FragmentFavoriteBinding? = null
+    private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -16,7 +16,7 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
     }
 

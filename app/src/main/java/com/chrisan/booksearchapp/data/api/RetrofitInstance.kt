54c03957af320b9599a60419df523be35ed25fdc
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-06 17:33:20 +0900

    [feat] : okhttp interceptor Retrofit api 구현

---
+++ app/src/main/java/com/chrisan/booksearchapp/data/api/RetrofitInstance.kt
@@ -0,0 +1,34 @@
+package com.chrisan.booksearchapp.data.api
+
+import com.chrisan.booksearchapp.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+/*
+    여러 개의 레트로핏 객체가 만들어지면, 자원도 낭비되며 통신에 혼선이 올수 있기 때문에 object 키워드를 사용 했고,
+    실제 사용하는 순간에 사용하게 끔 lazy 키워드를 사용했다.
+*/
+object RetrofitInstance {
+    private val okHttpClient: OkHttpClient by lazy {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+    val api: BookSearchApi by lazy {
+        retrofit.create(BookSearchApi::class.java)
+    }
+}

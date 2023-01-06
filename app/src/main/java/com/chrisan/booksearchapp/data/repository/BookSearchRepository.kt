2ba66fbcd0951f67f2c3b2387ed36b70a5a7c711
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-06 17:43:16 +0900

    [feat] : 책 검사해서 받아오는 데이터 reponse 를 리턴하는 repository 패턴 구현

---
+++ app/src/main/java/com/chrisan/booksearchapp/data/repository/BookSearchRepository.kt
@@ -0,0 +1,13 @@
+package com.chrisan.booksearchapp.data.repository
+
+import com.chrisan.booksearchapp.data.model.SearchResponse
+import retrofit2.Response
+
+interface BookSearchRepository {
+    suspend fun searchBooks(
+        query: String,
+        sort: String,
+        page: Int,
+        size: Int,
+    ): Response<SearchResponse>
+}

fc3850bd33c34a50b28028f6250fcc287c64b4c3
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-06 16:24:21 +0900

    [fix] : bottomnavigation view 동작 구현

--- app/src/main/java/com/chrisan/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/chrisan/booksearchapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.chrisan.booksearchapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.chrisan.booksearchapp.databinding.ActivityMainBinding
+import com.chrisan.booksearchapp.ui.view.FavoriteFragment
+import com.chrisan.booksearchapp.ui.view.SearchFragment
+import com.chrisan.booksearchapp.ui.view.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
@@ -12,5 +15,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupBottomNavigationView()
+
+        // App 이 처음 실행할 때만, search Fragment 화면 고정
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingsFragment())
+                        .commit()
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }

d7999443471aff04531020c1d34edb646e8cb94b
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-06 17:12:07 +0900

    [feat] : 상수 클래스 생성

---
+++ app/src/main/java/com/chrisan/booksearchapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.chrisan.booksearchapp.util
+
+import com.chrisan.booksearchapp.BuildConfig
+
+object Constants {
+    const val BASE_URL = "https://dapi.kakao.com/"
+    const val API_KEY = BuildConfig.bookApiKey
+}

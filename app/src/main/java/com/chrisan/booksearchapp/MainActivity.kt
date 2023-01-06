0a59ec64909520f6716a148dba3c048d4def57ba
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-06 15:44:15 +0900

    [refactor] : viewBinding 으로 코드 변경

--- app/src/main/java/com/chrisan/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/chrisan/booksearchapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.chrisan.booksearchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chrisan.booksearchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }

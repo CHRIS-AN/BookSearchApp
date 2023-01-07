64a895c21b1ef19afafcf1918e244beed52d8cb8
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-07 18:09:30 +0900

    [feat] : 책 검색 viewholder 구현

---
+++ app/src/main/java/com/chrisan/booksearchapp/ui/adapter/BookSearchViewHolder.kt
@@ -0,0 +1,24 @@
+package com.chrisan.booksearchapp.ui.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.chrisan.booksearchapp.data.model.Book
+import com.chrisan.booksearchapp.databinding.ItemBookPreviewBinding
+
+class BookSearchViewHolder(
+    private val binding: ItemBookPreviewBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(book: Book) {
+        val author = book.authors.toString().removeSurrounding("[", "]")
+        val publisher = book.publisher
+        val date = if (book.datetime.isNotEmpty()) book.datetime.substring(0, 10) else ""
+
+        itemView.apply {
+            binding.ivArticleImage.load(book.thumbnail)
+            binding.tvTitle.text = book.title
+            binding.tvAuthor.text = "$author | $publisher"
+            binding.tvDatetime.text = date
+        }
+    }
+}

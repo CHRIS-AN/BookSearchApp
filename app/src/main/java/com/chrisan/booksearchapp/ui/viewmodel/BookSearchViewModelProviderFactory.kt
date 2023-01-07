451455f053d3a561deb7980b0828e1f2efbc80cb
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-07 19:08:41 +0900

    [refactor] : viewmodel 초기화 할 때 매개변수가 추가되어, factory 코드 수정

--- app/src/main/java/com/chrisan/booksearchapp/ui/viewmodel/BookSearchViewModelProviderFactory.kt
+++ app/src/main/java/com/chrisan/booksearchapp/ui/viewmodel/BookSearchViewModelProviderFactory.kt
@@ -1,16 +1,26 @@
 package com.chrisan.booksearchapp.ui.viewmodel
 
+import android.os.Bundle
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.savedstate.SavedStateRegistryOwner
 import com.chrisan.booksearchapp.data.repository.BookSearchRepository
 
 class BookSearchViewModelProviderFactory(
-    private val bookSearchRepository: BookSearchRepository
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    private val bookSearchRepository: BookSearchRepository,
+    owner: SavedStateRegistryOwner,
+    defaultArgs: Bundle? = null,
+) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+    override fun <T : ViewModel> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ): T {
         if (modelClass.isAssignableFrom(BookSearchViewModel::class.java)) {
-            return BookSearchViewModel(bookSearchRepository) as T
+            return BookSearchViewModel(bookSearchRepository, handle) as T
         }
         throw java.lang.IllegalArgumentException("ViewModel class not found")
     }
+
 }

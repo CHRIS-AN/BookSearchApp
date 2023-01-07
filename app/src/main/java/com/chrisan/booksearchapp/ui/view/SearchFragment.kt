363ecc49707e6ac59b942491d3871d0fe21778f3
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-07 18:37:01 +0900

    [feat] : search fragment 에 mainActivity 에서 초기화한 viewModel 가져오기

--- app/src/main/java/com/chrisan/booksearchapp/ui/view/SearchFragment.kt
+++ app/src/main/java/com/chrisan/booksearchapp/ui/view/SearchFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.chrisan.booksearchapp.databinding.FragmentSearchBinding
+import com.chrisan.booksearchapp.ui.viewmodel.BookSearchViewModel
 
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var bookSearchViewModel: BookSearchViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +21,12 @@ class SearchFragment : Fragment() {
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // mainActivity 에서 초기화한 viewModel 을 가져온다.
+        bookSearchViewModel = (activity as MainActivity).bookSearchViewModel
     }
 
     override fun onDestroyView() {

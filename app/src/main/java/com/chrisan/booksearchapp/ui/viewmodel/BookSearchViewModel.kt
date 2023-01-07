0fba401028edaa2eddeca4c33bc1d4845596cddf
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-07 19:08:09 +0900

    [feat] : 앱 재시작 시, 쿼리 보존을 위해 savedState 구현

--- app/src/main/java/com/chrisan/booksearchapp/ui/viewmodel/BookSearchViewModel.kt
+++ app/src/main/java/com/chrisan/booksearchapp/ui/viewmodel/BookSearchViewModel.kt
@@ -1,16 +1,14 @@
 package com.chrisan.booksearchapp.ui.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.chrisan.booksearchapp.data.model.SearchResponse
 import com.chrisan.booksearchapp.data.repository.BookSearchRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class BookSearchViewModel(
-    private val bookSearchRepository: BookSearchRepository
+    private val bookSearchRepository: BookSearchRepository,
+    private val savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
     // api
@@ -24,5 +22,22 @@ class BookSearchViewModel(
                 _searchResult.postValue(body)
             }
         }
+    }
+
+    // SavedState (쿼리 보존에 사용할 변수)
+    var query = String()
+        set(value) { // backing field 를 사용하여, 쿼리가 변화되면 해당 값을 바로 반영하도록 함.
+            field = value
+            savedStateHandle[SAVE_STATE_KEY] = value
+        }
+
+    init {
+        // 초기화 할 때, 일단 savedState 에서 값을 가져오기.
+        query = savedStateHandle.get<String>(SAVE_STATE_KEY) ?: ""
+    }
+
+
+    companion object {
+        const val SAVE_STATE_KEY = "query" // savedState KEY
     }
 }

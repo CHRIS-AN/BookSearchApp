6a6005ad9fcc513d275ebb7d477ecbbd3a90d878
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-08 13:04:10 +0900

    [refactor] : room dao 가 사용할 crud 메소드 구현

--- app/src/main/java/com/chrisan/booksearchapp/data/repository/BookSearchRepository.kt
+++ app/src/main/java/com/chrisan/booksearchapp/data/repository/BookSearchRepository.kt
@@ -1,5 +1,7 @@
 package com.chrisan.booksearchapp.data.repository
 
+import androidx.lifecycle.LiveData
+import com.chrisan.booksearchapp.data.model.Book
 import com.chrisan.booksearchapp.data.model.SearchResponse
 import retrofit2.Response
 
@@ -10,4 +12,10 @@ interface BookSearchRepository {
         page: Int,
         size: Int,
     ): Response<SearchResponse>
+
+    suspend fun insertBooks(book: Book)
+
+    suspend fun deleteBooks(book: Book)
+
+    fun getFavoriteBooks(): LiveData<List<Book>>
 }

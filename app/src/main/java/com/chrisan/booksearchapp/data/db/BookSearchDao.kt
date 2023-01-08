934dddfe5663aca04abfbae186792ca268c39abc
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-08 12:15:30 +0900

    [feat] : book search dao 구현

---
+++ app/src/main/java/com/chrisan/booksearchapp/data/db/BookSearchDao.kt
@@ -0,0 +1,19 @@
+package com.chrisan.booksearchapp.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.chrisan.booksearchapp.data.model.Book
+
+@Dao
+interface BookSearchDao {
+    // OnConflictStrategy.REPLACE : 만약 동일한 primary key 값이 존재할 경우 덮어씌우기
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBook(book: Book)
+
+    @Delete
+    suspend fun deleteBook(book: Book)
+
+    // @Query 애노테이션은 시간이 덜 걸리기 때문에 코루틴 내에 비동기로 동작할 필요가 없다.
+    @Query("SELECT * FROM books")
+    fun getFavoriteBooks(): LiveData<List<Book>>
+}

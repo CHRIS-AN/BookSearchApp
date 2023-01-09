92d5fa0ca6281257b6ec213102a834bccaab7cd1
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-09 15:53:24 +0900

    [reafctor] : paging 을 loadState 와 adapter 연결

--- app/src/main/java/com/chrisan/booksearchapp/ui/view/SearchFragment.kt
+++ app/src/main/java/com/chrisan/booksearchapp/ui/view/SearchFragment.kt
@@ -5,7 +5,6 @@ import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
@@ -14,6 +13,7 @@ import androidx.paging.LoadState
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.chrisan.booksearchapp.databinding.FragmentSearchBinding
+import com.chrisan.booksearchapp.ui.adapter.BookSearchLoadStateAdapter
 import com.chrisan.booksearchapp.ui.adapter.BookSearchPagingAdapter
 import com.chrisan.booksearchapp.ui.viewmodel.BookSearchViewModel
 import com.chrisan.booksearchapp.util.Constants.SEARCH_BOOKS_TIME_DELAY
@@ -73,7 +73,12 @@ class SearchFragment : Fragment() {
                     DividerItemDecoration.VERTICAL
                 )
             )
-            adapter = bookSearchAdapter
+            //adapter = bookSearchAdapter
+
+            // pagingAdapter 와 loadStateAdapter 를 연결
+            adapter = bookSearchAdapter.withLoadStateFooter(
+                footer = BookSearchLoadStateAdapter(bookSearchAdapter::retry)
+            )
         }
 
         bookSearchAdapter.setOnItemClickListener {
@@ -123,21 +128,21 @@ class SearchFragment : Fragment() {
             binding.progressBar.isVisible = loadState.refresh is LoadState.Loading
 
             // 에러가 발생할 경우 토스트로 에러 표시
-            binding.btnRetry.isVisible = loadState.refresh is LoadState.Error
-                    || loadState.append is LoadState.Error
-                    || loadState.prepend is LoadState.Error
-
-            val errorState: LoadState.Error? = loadState.append as? LoadState.Error
-                ?: loadState.prepend as? LoadState.Error
-                ?: loadState.refresh as? LoadState.Error
-            errorState?.let {
-                Toast.makeText(requireContext(), it.error.message, Toast.LENGTH_SHORT).show()
-            }
+//            binding.btnRetry.isVisible = loadState.refresh is LoadState.Error
+//                    || loadState.append is LoadState.Error
+//                    || loadState.prepend is LoadState.Error
+//
+//            val errorState: LoadState.Error? = loadState.append as? LoadState.Error
+//                ?: loadState.prepend as? LoadState.Error
+//                ?: loadState.refresh as? LoadState.Error
+//            errorState?.let {
+//                Toast.makeText(requireContext(), it.error.message, Toast.LENGTH_SHORT).show()
+//            }
         }
         // retry 버튼 클릭 시, pagingAdapter 갱신
-        binding.btnRetry.setOnClickListener {
-            bookSearchAdapter.retry()
-        }
+//        binding.btnRetry.setOnClickListener {
+//            bookSearchAdapter.retry()
+//        }
     }
 
     override fun onDestroyView() {

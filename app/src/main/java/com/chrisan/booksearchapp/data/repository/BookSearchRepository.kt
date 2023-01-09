368c9940b9e910fbaef866160aa64222f5daf499
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-09 11:49:29 +0900

    [refactor] : Sort 기능에 필요한 메소드 추가 및 Datastore 를 사용하는 코드 구현

--- app/src/main/java/com/chrisan/booksearchapp/data/repository/BookSearchRepository.kt
+++ app/src/main/java/com/chrisan/booksearchapp/data/repository/BookSearchRepository.kt
@@ -18,4 +18,9 @@ interface BookSearchRepository {
     suspend fun deleteBooks(book: Book)
 
     fun getFavoriteBooks(): Flow<List<Book>>
+
+    // DataStore
+    suspend fun saveSortMode(mode: String)
+
+    suspend fun getSortMode(): Flow<String>
 }
--- app/src/main/java/com/chrisan/booksearchapp/data/repository/BookSearchRepositoryImpl.kt
+++ app/src/main/java/com/chrisan/booksearchapp/data/repository/BookSearchRepositoryImpl.kt
@@ -1,14 +1,25 @@
 package com.chrisan.booksearchapp.data.repository
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.chrisan.booksearchapp.data.api.RetrofitInstance.api
 import com.chrisan.booksearchapp.data.db.BookSearchDatabase
 import com.chrisan.booksearchapp.data.model.Book
 import com.chrisan.booksearchapp.data.model.SearchResponse
+import com.chrisan.booksearchapp.data.repository.BookSearchRepositoryImpl.PreferencesKeys.SORT_MODE
+import com.chrisan.booksearchapp.util.Sort
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import retrofit2.Response
+import java.io.IOException
 
 class BookSearchRepositoryImpl(
-    private val db: BookSearchDatabase
+    private val db: BookSearchDatabase,
+    private val dataStore: DataStore<Preferences>
 ) : BookSearchRepository {
     override suspend fun searchBooks(
         query: String,
@@ -29,5 +40,31 @@ class BookSearchRepositoryImpl(
 
     override fun getFavoriteBooks(): Flow<List<Book>> {
         return db.bookSearchDao().getFavoriteBooks()
+    }
+
+    private object PreferencesKeys {
+        val SORT_MODE = stringPreferencesKey("sort_mode")
+    }
+
+    override suspend fun saveSortMode(mode: String) {
+        dataStore.edit { prefs ->
+            prefs[SORT_MODE] = mode
+        }
+    }
+
+    // 파일 접근 실패시, catch block 안에서 처리하고, 통과 시, map black 안 flow<String> 을 반환 시킨다.
+    override suspend fun getSortMode(): Flow<String> {
+        return dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    exception.printStackTrace()
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { prefs ->
+                prefs[SORT_MODE] ?: Sort.ACCURACY.value // nulL 일경우에 기본 accuracy 이다.
+            }
     }
 }

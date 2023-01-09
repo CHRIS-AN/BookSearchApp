87dc5902a15657a985d773895e9d4c62361f7bdb
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-09 15:33:42 +0900

    [refactor] : SerachFragment 에 loadState 기능 구현

--- app/src/main/java/com/chrisan/booksearchapp/ui/view/SearchFragment.kt
+++ app/src/main/java/com/chrisan/booksearchapp/ui/view/SearchFragment.kt
@@ -5,9 +5,12 @@ import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.chrisan.booksearchapp.databinding.FragmentSearchBinding
@@ -41,6 +44,7 @@ class SearchFragment : Fragment() {
 
         setupRecyclerView()
         searchBooks()
+        setupLoadState()
 
         initObserve()
     }
@@ -99,6 +103,40 @@ class SearchFragment : Fragment() {
                 }
             }
             startItem = endTime // 시시각각 검색 내용을 보여주기 위해, 시작시간 초기화
+        }
+    }
+
+    // LoadState
+    private fun setupLoadState() {
+        bookSearchAdapter.addLoadStateListener { combinedLoadStates ->
+            val loadState = combinedLoadStates.source
+            // list 비어있는지 확인 item 1개 미만이면서, loadState 가 loading 되지 않고, 끝 쪽 page 가 아닐 경우
+            val isListEmpty = bookSearchAdapter.itemCount < 1
+                    && loadState.refresh is LoadState.NotLoading
+                    && loadState.append.endOfPaginationReached
+
+            // 검색 결과가 없을 경우, 'No result' 표시 후, recyclerview 가려주기
+            binding.tvEmptylist.isVisible = isListEmpty
+            binding.rvSearchResult.isVisible = !isListEmpty
+
+            // 검색 결과를 찾는 중에는 프로그래스바를 로딩하는 표시
+            binding.progressBar.isVisible = loadState.refresh is LoadState.Loading
+
+            // 에러가 발생할 경우 토스트로 에러 표시
+            binding.btnRetry.isVisible = loadState.refresh is LoadState.Error
+                    || loadState.append is LoadState.Error
+                    || loadState.prepend is LoadState.Error
+
+            val errorState: LoadState.Error? = loadState.append as? LoadState.Error
+                ?: loadState.prepend as? LoadState.Error
+                ?: loadState.refresh as? LoadState.Error
+            errorState?.let {
+                Toast.makeText(requireContext(), it.error.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+        // retry 버튼 클릭 시, pagingAdapter 갱신
+        binding.btnRetry.setOnClickListener {
+            bookSearchAdapter.retry()
         }
     }
 

e51bc17501d674783f1457e7c8ff56190d1f2c40
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-10 09:42:18 +0900

    [refactor] : Retrofit 을 Hilt 로 주입할 수 있게 리팩토링

---
+++ app/src/main/java/com/chrisan/booksearchapp/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.chrisan.booksearchapp.di
+
+import com.chrisan.booksearchapp.data.api.BookSearchApi
+import com.chrisan.booksearchapp.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    // Retrofit 
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .baseUrl(Constants.BASE_URL)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): BookSearchApi {
+        return retrofit.create(BookSearchApi::class.java)
+    }
+}

a30707eac1580e1b8b42b0765076b4155d2d5ada
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-10 10:14:56 +0900

    각 Fragment 에서 delegate 패턴으로 ViewModel 을 생성하게 끔 로직 수정

--- app/src/main/java/com/chrisan/booksearchapp/ui/viewmodel/BookSearchViewModelProviderFactory.kt
+++ app/src/main/java/com/chrisan/booksearchapp/ui/viewmodel/BookSearchViewModelProviderFactory.kt
@@ -1,28 +1,19 @@
 package com.chrisan.booksearchapp.ui.viewmodel
 
-import android.os.Bundle
-import androidx.lifecycle.AbstractSavedStateViewModelFactory
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.savedstate.SavedStateRegistryOwner
-import androidx.work.WorkManager
-import com.chrisan.booksearchapp.data.repository.BookSearchRepository
-
-class BookSearchViewModelProviderFactory(
-    private val bookSearchRepository: BookSearchRepository,
-    private val workManager: WorkManager,
-    owner: SavedStateRegistryOwner,
-    defaultArgs: Bundle? = null,
-) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
-    override fun <T : ViewModel> create(
-        key: String,
-        modelClass: Class<T>,
-        handle: SavedStateHandle
-    ): T {
-        if (modelClass.isAssignableFrom(BookSearchViewModel::class.java)) {
-            return BookSearchViewModel(bookSearchRepository, workManager, handle) as T
-        }
-        throw java.lang.IllegalArgumentException("ViewModel class not found")
-    }
-
-}
+//class BookSearchViewModelProviderFactory(
+//    private val bookSearchRepository: BookSearchRepository,
+//    private val workManager: WorkManager,
+//    owner: SavedStateRegistryOwner,
+//    defaultArgs: Bundle? = null,
+//) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
+//    override fun <T : ViewModel> create(
+//        key: String,
+//        modelClass: Class<T>,
+//        handle: SavedStateHandle
+//    ): T {
+//        if (modelClass.isAssignableFrom(BookSearchViewModel::class.java)) {
+//            return BookSearchViewModel(bookSearchRepository, workManager, handle) as T
+//        }
+//        throw java.lang.IllegalArgumentException("ViewModel class not found")
+//    }
+//}
